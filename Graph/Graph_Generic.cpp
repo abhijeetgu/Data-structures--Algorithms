4cc4a42194bd1dee4f023d18a226a656c5681ad4
Abhijeet Kumar <49158711+abhijeetgu@users.noreply.github.com>
2019-11-08 00:38:41 +0530

    Add files via upload

---
+++ Graph/Graph_Generic.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+#include<map>
+#include<list>
+using namespace std;
+
+template<typename T>
+class Graph{
+
+    map<T,list<T> > adjList;
+
+public:
+    Graph(){
+
+    }
+
+    void addEdge(T u,T v,bool bidir = true){
+       adjList[u].push_back(v);
+       if(bidir)
+       {
+           adjList[v].push_back(u);
+       }
+    }
+
+    void print()
+    {
+        //Iterate over the map
+        for(auto i:adjList){
+            cout<<i.first<<"->";
+
+            //i.second is LL
+            for (T entry:i.second){
+                cout<<entry<<",";
+            }
+            cout<<endl;
+        }
+
+
+    }
+
+};
+int main()
+{
+    Graph<string> g;
+    g.addEdge("Putin","Trump",false);
+    g.addEdge("Putin","Modi",false);
+    g.addEdge("Putin","Pope",false);
+    g.addEdge("Modi","Trump",true);
+    g.addEdge("Modi","Yogi",true);
+    g.addEdge("Yogi","Prabhu",false);
+    g.addEdge("Prabhu","Modi",false);
+    g.print();
+
+    cout<<endl<<endl;
+    Graph<int> g1;
+    g1.addEdge(1,2);
+    g1.addEdge(100,2);
+    g1.addEdge(200,2);
+    g1.addEdge(3,5);
+    g1.print();
+}
